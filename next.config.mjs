fdef69507a5fd10564ee6e066e15d81640619216
Rahul <110347707+Rahul-Sahani04@users.noreply.github.com>
2024-06-08 21:57:52 +0530

    chore: Update Next.js configuration for exporting static site

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,23 @@
 import { withContentlayer } from "next-contentlayer";
 // const { withContentlayer } = require("next-contentlayer");
 
+const isProd = process.env.NODE_ENV === 'production';
+
+module.exports = {
+  assetPrefix: isProd ? '/gaming-portfolio/' : '',
+  basePath: isProd ? '/gaming-portfolio' : '',
+  trailingSlash: true,
+};
+
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	pageExtensions: ["js", "jsx", "ts", "tsx", "md", "mdx", "glb", "gltf"],
 	experimental: {
-		mdxRs: true,
+	  mdxRs: true,
 	},
-};
+	output: 'export',
+  };
 
 
 export default withContentlayer(nextConfig);
