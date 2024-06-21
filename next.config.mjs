c5b36744dc222c489b485ae8e667c76434df8762
Rahul <110347707+Rahul-Sahani04@users.noreply.github.com>
2024-06-22 00:42:50 +0530

    chore: Remove unused CSS rule in CustomCursor.module.css and update Next.js configuration for exporting static site

--- next.config.mjs
+++ next.config.mjs
@@ -1,22 +1,14 @@
 import { withContentlayer } from "next-contentlayer";
 // const { withContentlayer } = require("next-contentlayer");
 
-const isProd = process.env.NODE_ENV === 'production';
-
-
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	assetPrefix: isProd ? '/gaming-portfolio/' : '',
-	basePath: isProd ? '/gaming-portfolio' : '',
-	trailingSlash: true,
-	// reactStrictMode: true,
+	output: "export",
 	pageExtensions: ["js", "jsx", "ts", "tsx", "md", "mdx", "glb", "gltf"],
 	experimental: {
-	  mdxRs: true,
+		mdxRs: true,
 	},
-	// output: 'export',
-  };
+};
 
 
 export default withContentlayer(nextConfig);
