8dd321300baaa8dda5dfbc187f01fc1f0b583432
Rahul-Sahani04 <110347707+Rahul-Sahani04@users.noreply.github.com>
2025-06-27 12:27:14 +0530

    Fix HTML entity for quotation marks in AboutPage

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -375,7 +375,7 @@ export default function AboutPage() {
 
             <DraggableCardContainer className="relative flex min-h-screen w-full items-center justify-center overflow-clip">
               <p className="absolute top-1/2 mx-auto max-w-sm -translate-y-3/4 text-center text-2xl font-black text-neutral-400 md:text-4xl dark:text-neutral-800">
-                "Photography is the story I fail to put into words."
+                &quot;Photography is the story I fail to put into words.&quot;
               </p>
               {photos.map((pic) => (
                 <DraggableCardBody key={pic.title} className={pic.className}>
