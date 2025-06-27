e099ebc72ac5111360f43e10c032b9fb7aa1f3d8
Rahul-Sahani04 <110347707+Rahul-Sahani04@users.noreply.github.com>
2025-06-27 12:57:29 +0530

    Add eslint configuration to next.config.mjs for build process

--- next.config.mjs
+++ next.config.mjs
@@ -3,8 +3,13 @@ import { withContentlayer } from "next-contentlayer";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	pageExtensions: ["js", "jsx", "ts", "tsx", "md", "mdx", "glb", "gltf"],
-	experimental: {
-		mdxRs: true,
-	},
+  pageExtensions: ["js", "jsx", "ts", "tsx", "md", "mdx", "glb", "gltf"],
+  experimental: {
+    mdxRs: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
+
+export default withContentlayer(nextConfig);
