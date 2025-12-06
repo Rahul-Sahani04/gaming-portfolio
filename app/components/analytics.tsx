070b49d8770dbae340b0c700acf81243c962d89e
Rahul-Sahani04 <110347707+Rahul-Sahani04@users.noreply.github.com>
2025-12-06 14:38:52 +0530

    feat: always load Beam Analytics with a hardcoded token and update Contentlayer imports to MJS.

--- app/components/analytics.tsx
+++ app/components/analytics.tsx
@@ -3,13 +3,21 @@
 export function Analytics() {
 	const token = process.env.NEXT_PUBLIC_BEAM_TOKEN;
 	if (!token) {
-		return null;
+		return (
+			<script
+				src="https://beamanalytics.b-cdn.net/beam.min.js"
+				data-token="a0fa7127-4c93-417b-abbe-b2165353b5f1"
+				async
+			>
+			</script>
+		)
 	}
 	return (
 		<script
 			src="https://beamanalytics.b-cdn.net/beam.min.js"
-			data-token={token}
+			data-token="a0fa7127-4c93-417b-abbe-b2165353b5f1"
 			async
-		/>
+		>
+		</script>
 	);
 }
