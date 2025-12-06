f26d84b78b88983b97e58598b91d9177c204f195
Rahul-Sahani04 <110347707+Rahul-Sahani04@users.noreply.github.com>
2025-12-06 14:47:34 +0530

    refactor: Delete core `src/app` files and update analytics script to use `next/script` and environment variable.

--- app/components/analytics.tsx
+++ app/components/analytics.tsx
@@ -1,23 +1,16 @@
 "use client";
+import Script from "next/script";
 
 export function Analytics() {
 	const token = process.env.NEXT_PUBLIC_BEAM_TOKEN;
 	if (!token) {
-		return (
-			<script
-				src="https://beamanalytics.b-cdn.net/beam.min.js"
-				data-token="a0fa7127-4c93-417b-abbe-b2165353b5f1"
-				async
-			>
-			</script>
-		)
+		return null;
 	}
 	return (
-		<script
+		<Script
 			src="https://beamanalytics.b-cdn.net/beam.min.js"
-			data-token="a0fa7127-4c93-417b-abbe-b2165353b5f1"
-			async
-		>
-		</script>
+			data-token={token}
+			strategy="afterInteractive"
+		/>
 	);
 }
