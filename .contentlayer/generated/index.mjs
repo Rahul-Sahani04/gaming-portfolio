296bdd2e0c4ed1fcfeae4006df74aa61c8805c5e
Rahul-Sahani04 <110347707+Rahul-Sahani04@users.noreply.github.com>
2026-04-16 16:15:23 +0530

    feat: update CustomCursor component to use refs for improved performance and simplify event handling

--- .contentlayer/generated/index.mjs
+++ .contentlayer/generated/index.mjs
@@ -4,9 +4,9 @@ export { isType } from 'contentlayer2/client'
 
 // NOTE During development Contentlayer imports from `.mjs` files to improve HMR speeds.
 // During (production) builds Contentlayer it imports from `.json` files to improve build performance.
-import { allPages } from './Page/_index.mjs'
-import { allProjects } from './Project/_index.mjs'
-import { allBlogs } from './Blog/_index.mjs'
+import allPages from './Page/_index.json' with { type: 'json' }
+import allProjects from './Project/_index.json' with { type: 'json' }
+import allBlogs from './Blog/_index.json' with { type: 'json' }
 
 export { allPages, allProjects, allBlogs }
 
