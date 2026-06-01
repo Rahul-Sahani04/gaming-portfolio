0e1770ec4ea99dfd4ef5138a7b8edc37edcbb82f
Rahul Sahani <110347707+Rahul-Sahani04@users.noreply.github.com>
2026-06-01 20:29:34 +0530

    fix: cast RetroEffect component and update Contentlayer imports to use json modules with assertions

--- app/components/react-bits/Dither.tsx
+++ app/components/react-bits/Dither.tsx
@@ -159,7 +159,7 @@ class RetroEffectImpl extends Effect {
 
 const RetroEffect = forwardRef<RetroEffectImpl, { colorNum: number; pixelSize: number }>((props, ref) => {
   const { colorNum, pixelSize } = props;
-  const WrappedRetroEffect = wrapEffect(RetroEffectImpl);
+  const WrappedRetroEffect = wrapEffect(RetroEffectImpl as any);
   return <WrappedRetroEffect ref={ref} colorNum={colorNum} pixelSize={pixelSize} />;
 });
 
